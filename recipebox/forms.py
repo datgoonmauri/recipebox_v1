819abcf802d7a300b827b55d84d814b0372cfb6b
detrich <detrich2k@gmail.com>
2020-05-04 13:02:22 -0400

    updated charfield times in the form to match the model

--- recipebox/forms.py
+++ recipebox/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from recipebox.models import Author
 
 class AddRecipeForm(forms.Form):
-    title = forms.CharField(max_length=30)
+    title = forms.CharField(max_length=40)
     description = forms.CharField(widget=forms.Textarea)
     instructions = forms.CharField(widget=forms.Textarea)
-    timereq = forms.CharField(max_length=30)
+    timereq = forms.CharField(max_length=15)
     author = forms.ModelChoiceField(queryset=Author.objects.all())
 
 class AddAuthorForm(forms.ModelForm):
